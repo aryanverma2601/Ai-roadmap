ec44c93414fdb78c757263081365f62c15b94bbc
aryanverma2601 <aryanv380@gmail.com>
2025-07-31 15:16:29 +0530

    Fixing Bugs

--- Backend/02/script.js
+++
@@ -1,9 +0,0 @@
-//node js core mei jo installed aate hai wo hote hai moudle
-// npm se download krte hai woh hote hai package
-
-//install -> npm i packageName
-//for particular version npm i packageName@version
-//uninstall -> npm uninstall packageName
-
-console.log("Started");
-
--- Backend/04/script4.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const app = express();
-
-//for server to read
-app.use(express.json()); //json based data readable
-app.use(express.urlencoded({ extended: true }));
